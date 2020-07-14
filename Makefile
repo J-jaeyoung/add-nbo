d15537ce892bf0c1ffd7a2c90f2f107bbe4ad33f
J-jaeyoung <jjy600901@snu.ac.kr>
2020-07-14 13:54:14 +0900

    Complete without .bin

---
+++ Makefile
@@ -0,0 +1,10 @@
+all: add-nbo 
+
+add-nbo: add-nbo.o
+	g++ add-nbo.o -o add-nbo
+
+add-nbo.o: add-nbo.cpp
+	g++ -c add-nbo.cpp -o add-nbo.o
+
+clean:
+	rm -rf *.o add-nbo
--- add-nbo.cpp
+++ add-nbo.cpp
@@ -1,2 +1,27 @@
 #include <stdio.h>
+#include <netinet/in.h>
+#include <stdint.h>
 
+int main(int argc, char *argv[]){
+    uint32_t first, second, sum;
+
+    if(argc != 3){
+        printf("Usage: %s <file1> <file2>\n", argv[0]);
+        return 0;
+    }
+
+    FILE *f1 = fopen(argv[1], "r");
+    FILE *f2 = fopen(argv[2], "r");
+
+    fread(&first, 1, 4, f1);
+    fread(&second, 1, 4, f2);
+
+    first = htonl(first);
+    second = ntohl(second);
+    sum = first + second;
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", 
+            first, first, second, second, sum, sum);
+
+    fclose(f2);
+    fclose(f1);
+}
